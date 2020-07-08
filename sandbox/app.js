d68aa5514ca6964e7715ea2b953418742c6352b0
Siege Teodosio <siegeteodosio15@gmail.com>
2020-07-08 21:14:30 +0800

    update button

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,7 +1,7 @@
 const carouselSlide = document.querySelector(".carousel-slide");
 const carouselImages = document.querySelectorAll(".carousel-tile");
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".btn-prev");
+const nextBtn = document.querySelector(".btn-next");
 
 //translate image from showing last image clone to the actual first image
 let counter = 1; //pertains to first image
@@ -12,12 +12,13 @@ carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 
 //event listeners
 nextBtn.addEventListener("click", () => {
-  //if (counter <= 0) return;
+  if (counter >= carouselImages.length - 1) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter++;
   carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
 prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter--;
   carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
